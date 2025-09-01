05199b24c91a395436699ecd6e3e663eea06530f
Tonny Huynh <tonnyhuynh02@gmail.com>
2025-09-01 18:14:45 +0200

    70 lines of code added

--- 02_tourofpython.py
+++ 02_tourofpython.py
@@ -95,14 +95,76 @@ for word in iliad_opening:
 print(initial)
 '''
 
+'''
 # Slices
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 first = alphabet[0:3]
 second = alphabet[:3] #Samma som first
 third = alphabet[3:6] # index 3 till 5 så: 'def'
 fourth = alphabet[-3:] # 'xyz'
+fifth = alphabet[10:-10]  # 'klmnop'
+
 
 print(first)  # 'abc'
 print(second) # 'abc'
 print(third)  # 'def'
 print(fourth) # 'xyz'
+print(fifth)  # 'klmnop'
+print(alphabet[:]) # Hela alfabetet
+
+'''
+
+# The whole string
+'''
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+i = 10
+alphabet[:i] + alphabet[i:]  # 'abcdefghijklmnopqrstuvwxyz'
+print(alphabet[:i] + alphabet[i:]) # 'abcdefghijklmnopqrstuvwxyz'
+
+print(alphabet[0::2]) # 'acegikmopqsuwy'
+'''
+
+# Special Characters
+
+'''
+print('Python\'s strings') #Samma som under
+print("Python's strings")
+print('\N{Commercial AT}') # @
+print('\x40') # @, Använder UTF-8 standard
+print('\u0152') # 'Œ'
+
+# r som prefix för att behandla backslashes som vanliga tecken/karaktärer
+print(r'\N{COMMERCIAL AT}')  # '\\N{COMMERCIAL AT}'
+print(r'\x40') # @, Använder UTF-8 standard
+print(r'\u0152') # 'Œ'
+
+'''
+# Formatting strings
+'''
+begin = 'my'
+print('{} string {}'.format(begin, 'is empty')) # 'my string is empty'
+'''
+
+# Data identifies and types
+
+'''
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+print(12)
+print(type(12)) # <class 'int'>
+print(id(12)) # Jag fick inte outputen 4305677768
+
+a = 12
+print(id(a))
+print(type(a))
+print(type(12.0))  # <class 'float'>
+print(type(True))  # <class 'bool'>
+print(type(1 < 2))  # <class 'bool'>
+print(type(None))  # <class 'NoneType'>
+b = '12'
+print(id(b))
+print(type('12'))
+print(id(alphabet))
+print(type(alphabet))  # <class 'str'>
+'''
+
+# Type conversions

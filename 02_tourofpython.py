eed293aab3193f91d1ad2fda68efeae5e70d7dc0
Tonny Huynh <tonnyhuynh02@gmail.com>
2025-09-02 14:57:06 +0200

    LEarning python data structures

--- 02_tourofpython.py
+++ 02_tourofpython.py
@@ -232,7 +232,102 @@ print(list4[1][3])  # 'Python', Så det funkar via att man tar första andra ele
 list5 = list2
 [v1, v2, v3] = list5 # Unpacking variables and their values
 print([v1, v2, v3])
-'''
+
 
 
 # List Copy
+print(id(list2))
+print(list2)
+print(list5)
+list6 = list2.copy()
+print(id(list6))
+
+#Identity and equality
+print(list2 == list5)
+print(list2 == list6)
+print(list2 is list5)
+print(list2 is list6)
+
+list2[1] = 2
+print(list2)
+print(list5)
+print(list6)
+print(id(list2))
+
+#Deep Copy
+print(id(list4.copy()[0]))
+
+import copy
+print(id(copy.deepcopy(list4)[0]))
+
+#List operations and functions
+print(list3[:])
+print(list3[:-1])
+print([1, 2, 3] + ['a', 'b'])  # [1, 2, 3, 'a', 'b']
+print(list2[:2] + list3[2:-1])
+print(list2*2)
+print([0.0]*4)
+
+print(len(list2))  # 3
+
+list2.extend([4, 5])  # [1, 2, 3, 4, 5]
+print(list2)
+
+list2.append(6)  # [1, 2, 3, 4, 5, 6]
+print(list2)
+
+list2.append([7, 8])  # [1, 2, 3, 4, 5, 6, [7, 8]]
+print(list2)
+
+list2.pop(-1)  # [1, 2, 3, 4, 5, 6]
+print(list2)
+
+
+list2.remove(1)  # [2, 3, 4, 5, 6]
+print(list2)
+
+list2.insert(0, 'a')  # ['a', 2, 3, 4, 5, 6]
+print(list2)
+
+print(list5)
+print(list6)
+
+'''
+
+#Tuples
+
+'''
+Tuple1 = () # Empty tuple
+tuple1 = tuple()  # Another way to create an empty tuple
+tuple2 = (1, 2, 3, 4)
+print(tuple2)
+print(tuple2[3])
+
+tuple2[1:4]  # (2, 3, 4)
+
+#tuple2[3] = 8  # Type error: Tuples are immutable
+list7 = ['a', 'b', 'c']
+tuple3 = tuple(list7)  # conversion to a tuple: ('a', 'b', 'c')
+print(tuple3)
+
+print(type(tuple3))  # <class 'tuple'>
+
+list8 = list(tuple2)  # [1, 2, 3, 4]
+print(tuple([1]))
+
+print(list((1,)))
+
+tuple4 = (tuple2, list7)  # ((1, 2, 3, 4), ['a', 'b', 'c'])
+print(tuple4[0])  # (1, 2, 3, 4),
+
+tuple4[1]  # ['a', 'b', 'c']
+
+print(tuple4[0][2])  # 3
+
+print(tuple4[1][1])  # 'b'
+
+tuple4[1][1] = 'β'  # ((1, 2, 3, 4), ['a', 'β', 'c'])
+print(tuple4)
+'''
+
+# Sets

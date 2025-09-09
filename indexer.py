ca48bcf487c4ed7def728dbb7465b5410cd8baf6
Tonny Huynh <tonnyhuynh02@gmail.com>
2025-09-10 00:44:02 +0200

    finalized lab

--- indexer.py
+++ indexer.py
@@ -44,7 +44,7 @@ text = open('Selma/bannlyst.txt').read()
 index = dict() # Skapa en dictionary
 wholeText = re.finditer(r'\p{L}+', text)
 for match in wholeText: # För varje match i texten, L för letters då vi inte vill ha andra symboler etc. Lägga till + för att få med hela ordet
-    word = match.group().lower() # Varje match kör vi till lower case
+    word = match.group().lower() # Varje match kör vi till lower case // group() metoden returnerar en del av regexet, dvs en sträng
     position = match.start() # hämtar positionen av ordet
     if word not in index: # Lägga till ordet i vår dictionary för första gången
         index[word] = []
@@ -122,7 +122,7 @@ textMårbacka = read_file_and_tokenize_and_index_it(file = 'Selma/marbacka.txt',
 
 # Reading the content of a folder
 
-def get_files(dir, suffix):
+def get_files(dir = 'Selma/', suffix = 'txt'):
     """
     Returns all the files in a folder ending with suffix
     :param dir:
@@ -204,11 +204,15 @@ idf, the logarithm base 10 of the inverse document frequency. """
 
 # Tf will be the relative frequency of the term in the document
 def tfCalculations(word, file):
+    # Öppna texten
     text = open(file, 'r', encoding='utf-8').read().lower() 
-
-    index = [tokenize(text)]
-    frequencyOfTerm = index.count(word)
-    totalNbrWords = len(index)
+    # Tokenisera texten
+    index = tokenize(text)
+    tokens = []
+    for match in index:
+        tokens.append(match.group())
+    frequencyOfTerm = tokens.count(word)
+    totalNbrWords = len(tokens)
 
     result = frequencyOfTerm/totalNbrWords
     if(result < 0.0):
@@ -218,23 +222,109 @@ def tfCalculations(word, file):
    
 # idf, the logarithm base 10 of the inverse document frequency.
 def idfCalculations(word, master_index):
-    # Vi måste iterera genom alla dokument
-    files = get_files('Selma/', 'txt')
-    N = 0
-    for file in files:
-        N += 1
-    # Addera alla våra files, dvs N
+    total_docs = len(get_files('Selma/', 'txt'))
+        
+    if word not in master_index:
+        return 0  # ordet finns inte i något dokument
+    d_t = len(master_index[word])  # antal dokument där ordet finns
+    return math.log10(total_docs / d_t)
 
-    # Nu behöver vi räkna ankomsten av ordet d_T
-    d_t = len(master_index[word])
-
-    return math.log10(N/d_t)
-
+# According to the wikipedia page: Term frequency–inverse document frequency slide
 def calculating_id_tf(word, file, master_index):
-   idf = idfCalculations(word, master_index)
-   tf = tfCalculations(word, file)
-   return idf * tf
+    idf = idfCalculations(word, master_index)
+    tf = tfCalculations(word, file)
+    return tf * idf
 
-master_Index = readAllFilesTokenizeAndIndexAll('Selma/', 'txt', 'samlar')
 
-# calculating_id_tf('samlar', file = 'Selma/marbacka.txt', master_Index)
+master_Indexx = readAllFilesTokenizeAndIndexAll('Selma/', 'txt', r'\p{L}+')
+
+result = calculating_id_tf(word='nils', file='Selma/marbacka.txt', master_index=master_Indexx)
+resul2 = calculating_id_tf(word='et', file='Selma/marbacka.txt', master_index=master_Indexx)
+resul3 = calculating_id_tf(word='nils', file='Selma/jerusalem.txt', master_index=master_Indexx)
+
+
+
+print('idtf results:', result)
+print('idtf results:', resul2)
+print('correct, now to the jerusalem.txt', resul3)
+
+# Creating a method for the whole dictionary
+def id_if_forEachDocument(master_index, dir='Selma/'):
+    tfidf = {}  # Dictionary for all documents
+
+    files = get_files(dir, 'txt')
+    all_words = master_index.keys()  # All unique words in corpus as each word is branded as a key while the position as value
+
+    for file in files:
+        tfidf[file] = {}
+        for word in all_words:
+            text = os.path.join(dir, file)
+            tfidf[file][word] = calculating_id_tf(word, text, master_index)
+    return tfidf
+
+print('idif for the whole dictionary for each file')
+print(id_if_forEachDocument(master_Indexx))
+
+# Comparing Documents
+    # Cosine similarity
+# According to lecture chapter 9 for the cosine is the dot product D1*D"/ Absolute value of D1 multiplied by absolut value of D2
+
+def cosine_similarity(document1, document2):
+    # Both documents should have the same key when comparing the cosine similarity
+    all_words = []
+    for word in document1:
+        for comparableWord in document2:
+            if word == comparableWord:
+                all_words.append(word)
+
+    dot_product = 0
+    D1 = 0
+    D2 = 0
+
+    for word in all_words:
+        tfidf_Value_1 = document1.get(word, 0) # returns the value associated with the word, but if that word does not have a value, the default return value on that key is 0
+        tfidf_Value_2 = document2.get(word, 0)
+
+        dot_product += tfidf_Value_1 * tfidf_Value_2
+
+        D1 += tfidf_Value_1**2
+        D2 += tfidf_Value_2**2
+    
+    D1 = math.sqrt(D1)
+    D2 = math.sqrt(D2)
+
+    if D1 == 0 or D2 == 0:
+        return 0
+
+    return dot_product / (D1 * D2)
+
+def similarityMatrix():
+    files = get_files()
+    tfidf_forAllDocuments = id_if_forEachDocument(master_Indexx)
+    similarity_matrix = {}
+    most_sim_doc1 = ""
+    most_sim_doc2 = ""
+    max_similarity = 0
+
+    for file1 in files:
+        similarity_matrix[file1] = {}
+        for file2 in files:
+             # Calculate cosine similarity
+            similarity = cosine_similarity(tfidf_forAllDocuments[file1], tfidf_forAllDocuments[file2])
+            similarity_matrix[file1][file2] = similarity
+
+            # Update most similar pair 
+            if file1 != file2:
+                if similarity > max_similarity:
+                    max_similarity = similarity
+                    most_sim_doc1 = file1
+                    most_sim_doc2 = file2
+    
+    print("Most similar:", most_sim_doc1, most_sim_doc2, "Similarity:", max_similarity)
+    return similarity_matrix, most_sim_doc1, most_sim_doc2, max_similarity
+
+
+
+
+
+

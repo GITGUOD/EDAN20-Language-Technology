1624c72528aaa24d4dc14a74706b3a48dcce279e
Tonny Huynh <tonnyhuynh02@gmail.com>
2025-09-16 17:22:10 +0200

    Last part of the lab

--- LanguageModel.py
+++ LanguageModel.py
@@ -41,19 +41,6 @@ def countUniqueWordsOfText(text):
 # You will write a program to tokenize your text, insert <s> and </s> tags to delimit sentences, and set all the words in lowercase letters.
 # In the end, you will only keep the words.
 
-def segmentingCorpus(text):
-    words = tokenize(r'\p{L}+', text)
-    s_end = ['.', ';', ':','?', '!']
-    segmented = ['<s>']
-
-    for word in words:
-        segmented.append(word.lower())
-        if word in s_end:
-            segmented.append('</s>')
-            segmented.append('<s>')
-    return segmented
-
-# print(len(segmentingCorpus(corpus)))
 
 # Non-letter nor the following punctations
 nonletter = r'[^\p{L}+\.;:?!]'
@@ -85,18 +72,116 @@ sentence_markup = re.sub(sentence_boundaries, sentence_markup, test_para)
 
 # Applying substitution:
 text = sentence_markup
-print(text)
+#print(text)
 
 text = '<s>' + sentence_markup + '</s>' # We insert the s or s:es due to the substitution only works between sentences, not at the start nor end
-print(text)
+#print(text)
 
 # Replace the space duplicates with one space and remove the punctuation signs. For the spaces, use the Unicode regex. You may use \s though.
 
-text = ..
+#Removing space duplicates:
+Sduplicates = r'\s+'
 
-print(text)
+textWithoutDuplicateSpaces = re.sub(Sduplicates, ' ', text)
+
+#print(textWithoutDuplicateSpaces)
+
+# Removing punctuation signs
+
+# Match punctuation characters: . ; : ? !
+punctuationRemoval = r"[.;:?!]"
+
+textWithoutPunctuation = re.sub(punctuationRemoval, '', textWithoutDuplicateSpaces)
+# print(textWithoutPunctuation)
 
 
+def segment_sentences(text):
+    text = text.lower()
+
+    sentence_boundaries = r"([.!?])(\s+)(\p{L})"
+
+    sentence_markup = r"\1 </s>\n<s> \3"
+
+    text = re.sub(sentence_boundaries, sentence_markup, text)
+
+    p_removal = r"[.;:?!]"
+
+    text = re.sub(p_removal, '', text)
+
+    Sduplicates = r'\s+'
+
+    text = re.sub(Sduplicates, ' ', text)
 
 
+    text = " <s> " + text + " </s> "
 
+    return text
+
+#print(segment_sentences(test_para))
+
+
+# Accuracy? Hm, jag tror den har ganska bra accuracy men tar inte hänsyn till t.ex citat eller liknande så som förkortningar etc.
+
+def segmenting_corpus(text):
+    text = clean(text)
+    text = segment_sentences(text)
+    return text
+#print("new line")
+s = segmenting_corpus(corpus[-557:])
+
+#print(s)
+
+# Fel i uppgiften? Vi får en extra mening i början.
+
+# List of Words
+def createAListOfWords(text):
+    '''
+    Split metoden gör om strängen till en lista
+    '''
+    return text.split()
+
+newWords = createAListOfWords(segmenting_corpus(corpus))
+# print(newWords)
+
+
+#print(segmenting_corpus(corpus[-557:]))
+
+# Counting unigrams and bigrams
+
+# Unigrams
+def unigrams(words):
+    frequency = {}
+    for i in range(len(words)):
+        if words[i] in frequency:
+            frequency[words[i]] += 1
+        else:
+            frequency[words[i]] = 1
+    return frequency
+
+frequency = unigrams(newWords)
+list(frequency.items())[:20]
+print("unigrams")
+
+print(list(frequency.items())[:20])
+
+print()
+print("bigrams")
+
+
+# Bigrams
+def bigrams(words):
+    bigrams = []
+    for i in range(len(words) - 1):
+        bigrams.append((words[i], words[i + 1]))
+    frequency_bigrams = {}
+    for i in range(len(words) - 1):
+        if bigrams[i] in frequency_bigrams:
+            frequency_bigrams[bigrams[i]] += 1
+        else:
+            frequency_bigrams[bigrams[i]] = 1
+    return frequency_bigrams
+frequency_bigrams = bigrams(newWords)
+print(list(frequency_bigrams.items())[:20])
+
+# In the report, tell what is the possible number of bigrams and their real number?
+# Explain why such a difference. What would be the possible number of 4-grams.
